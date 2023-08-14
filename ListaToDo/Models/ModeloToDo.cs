14b7bdf52b50a51cfa62e9e32e66301906cd26c9
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-08-14 16:24:27 -0300

    Arrumando bugs e desenvolvendo os relacionamentos entre usuário e perfill

---
+++ ListaToDo/Models/ModeloToDo.cs
@@ -0,0 +1,7 @@
+namespace Models.ToDo {
+    public class ToDo {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
